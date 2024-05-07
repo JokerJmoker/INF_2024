37d0d0f12f57c963eb8cd31045cce6d23d9c2ce1
JokerJmoker <storos61@mail.ru>
2024-05-07 14:05:21 +0300

    вроде работает

--- LAB_2/dock_3/create_db.py
+++ LAB_2/dock_3/create_db.py
@@ -1,18 +1,13 @@
 from pymongo import MongoClient
 
-# создаем объект , указываю адрес и порт 
-client = MongoClient('mongodb://mongodb_container:27017/')
-
+client = MongoClient('mongo', 27017)
 db = client['testdb']
 collection = db['collection']
 
-# вставка документов в коллекцию
-for i in range(6):
-    document = {"tags": f"obj_{i}"}
-    result = collection.insert_one(document)
-    print(f"New document {i} id:", result.inserted_id)
+documents = [{"tags": f"obj_{i}"} for i in range(6)] # генератор
+result = collection.insert_many(documents)
 
-# вывод бд
+# Вывод бд
 documents = collection.find({})
 print("Documents found:")
 for doc in documents:
